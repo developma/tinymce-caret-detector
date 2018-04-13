18be8f9e97d0585891a0e456b24372c3269f2c5a
developma <developer.daisuke.maeno@gmail.com>
2018-04-13 23:41:57 +0900

    add showing current node

--- tinymce-config.js
+++ tinymce-config.js
@@ -8,19 +8,52 @@ class TinyMCEConfig {
     tinymce.init({
       selector: '#mytextarea',
       setup: editor => {
-        editor.on('KeyUp', e => caretDetector.getCurrentPosition(editor, e))
+        editor.on('KeyUp', e => caretDetector.showInfo(editor, e))
       }
     })
   }
 }
 
 class CaretDetector {
+  showInfo(editor, e) {
+    this.showPosition(this.getCurrentPosition(editor, e))
+    this.showCurrentNodeLocation(editor)
+  }
+  showCurrentNodeLocation(editor) {
+    document.getElementById('location').value = this.getCurrentNodeLocation(editor, this.getCurrentNode(editor))
+  }
+  showPosition(position) {
+    const {startOffset, endOffset} = position
+    document.getElementById('startOffset').value = startOffset
+    document.getElementById('endOffset').value = endOffset
+  }
   getCurrentPosition(editor, e) {
     const { startOffset, endOffset } = editor.selection.getRng()
-    const startElem = document.getElementById('startOffset')
-    startElem.value = startOffset
-    const endElem = document.getElementById('endOffset')
-    endElem.value = endOffset
+    return { startOffset, endOffset }
+  }
+  getRootNode(editor) {
+    return editor.getBody()
+  }
+  getCurrentNode(editor) {
+    return editor.selection.getNode()
+  }
+
+  getCurrentNodeLocation(editor, currentNode) {
+    const rootPNodes = this.getRootNode(editor).children
+    let location = 0
+    Array.from(rootPNodes).forEach((rootPNode, idx) => {
+      if (rootPNode === this.getPNode(currentNode)) {
+        location = idx
+      }
+    })
+    return location
+  }
+
+  getPNode(node) {
+    while (node.nodeName !== 'P') {
+      node = getPNode(node.parentNode)
+    }
+    return node
   }
 }
 
